71d616392b1d902c7be529c0dd8f3113bebf80b3
Brett herrell <brett_herrell@yahoo.com>
2021-10-22 17:00:28 -0500

    added get path and post path for api's

--- Develop/server.js
+++ Develop/server.js
@@ -5,9 +5,14 @@ const router = require("express").Router();
 const app = express();
 const fs = require("fs");
 const path = require("path");
+const { notes } = require("./db/db");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static(__dirname + "/public"));
-app.get("/", (req, res) => {
+
+router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
@@ -15,6 +20,22 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
+app.get("/api/notes", (req, res) => {
+  return res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+  const what = req.body;
+
+  console.log(what);
+
+  notes.push(what);
+
+  res.json(notes);
+});
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
+
+module.exports = router;
